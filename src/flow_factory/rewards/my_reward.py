ae86dd73b97c290f49cf3c3a1acd74d4ac9bbd66
Jayce-Ping <315229706@qq.com>
2026-01-08 13:05:24 +0800

    Update type

--- src/flow_factory/rewards/my_reward.py
+++ src/flow_factory/rewards/my_reward.py
@@ -1,7 +1,7 @@
 # src/flow_factory/rewards/my_reward.py
 from accelerate import Accelerator
 from transformers import CLIPProcessor, CLIPModel
-from typing import Optional, List
+from typing import Optional, List, Union
 from PIL import Image
 from contextlib import nullcontext
 import torch
@@ -28,7 +28,7 @@ class MyRewardModel(BaseRewardModel):
         prompt : List[str],
         image : Optional[List[Image.Image]] = None,
         video : Optional[List[List[Image.Image]]] = None,
-        condition_images: Optional[List[List[Image.Image]]] = None,
+        condition_images: Optional[List[Union[List[Image.Image], torch.Tensor]]] = None,
         **kwargs,
     ) -> RewardModelOutput:
         """
@@ -37,9 +37,11 @@ class MyRewardModel(BaseRewardModel):
             prompt (list[str]): List of text prompts.
             image (list[Image.Image]): List of generated images corresponding to the prompts.
             video (list[list[Image.Image]]): List of generated videos (each video is a list of frames) corresponding to the prompts.
-            condition_images (Optional[List[List[Image.Image]]]): Optional list of condition images
-                (each element is a list of images. If only one condition image per prompt, this will be a list of single-element lists).
+            condition_images (Optional[List[List[Image.Image] | torch.Tensor]]): Optional list of condition images
+                - each element is a list of images. If only one condition image per prompt, this will be a list of single-element lists.
+                - each element is a tensor with batch dimension, scaled in [0, 1].
         Returns:
+            RewardModelOutput: Contains rewards tensor and any extra information.
         """
 
         # Ensure inputs are lists, each of length reward_args.batch_size
